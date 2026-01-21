1f8bb181f23e6aebecc609226ca9eb9ad8ee21bf
Tyler Richmond <tyler.richmond@frasers.group>
2026-01-21 10:40:04 +0000

    cookie fix

--- src/main/java/main/api/AuthController.java
+++ src/main/java/main/api/AuthController.java
@@ -41,14 +41,14 @@ public class AuthController {
     sessionCookie.setSecure(true);
     sessionCookie.setPath("/");
     sessionCookie.setMaxAge(1 * 60); // 1 minutes
-    sessionCookie.setDomain(".george.richmond.gg");
+    sessionCookie.setDomain("george.richmond.gg");
    
     Cookie refreshCookie = new Cookie("REFRESH", refreshToken);
     refreshCookie.setHttpOnly(true);
     refreshCookie.setSecure(true);
     refreshCookie.setPath("/auth/refresh"); // only sent to refresh endpoint
     refreshCookie.setMaxAge(2 * 60); // 2 minutes
-    refreshCookie.setDomain(".george.richmond.gg");
+    refreshCookie.setDomain("george.richmond.gg");
 
     response.addCookie(sessionCookie);
     response.addCookie(refreshCookie);
@@ -70,14 +70,14 @@ public class AuthController {
     sessionCookie.setSecure(true);
     sessionCookie.setPath("/");
     sessionCookie.setMaxAge(1 * 60); // 1 minutes
-    sessionCookie.setDomain(".george.richmond.gg");
+    sessionCookie.setDomain("george.richmond.gg");
    
     Cookie refreshCookie = new Cookie("REFRESH", refreshToken);
     refreshCookie.setHttpOnly(true);
     refreshCookie.setSecure(true);
     refreshCookie.setPath("/auth/refresh"); // only sent to refresh endpoint
     refreshCookie.setMaxAge(2 * 60); // 2 minutes
-    refreshCookie.setDomain(".george.richmond.gg");
+    refreshCookie.setDomain("george.richmond.gg");
 
     response.addCookie(sessionCookie);
     response.addCookie(refreshCookie);
@@ -122,14 +122,14 @@ public class AuthController {
         sessionCookie.setSecure(true);
         sessionCookie.setPath("/");
         sessionCookie.setMaxAge(1 * 60); // 1 minutes
-        sessionCookie.setDomain(".george.richmond.gg");
+        sessionCookie.setDomain("george.richmond.gg");
       
         Cookie refreshCookie = new Cookie("REFRESH", newRefreshToken);
         refreshCookie.setHttpOnly(true);
         refreshCookie.setSecure(true);
         refreshCookie.setPath("/auth/refresh"); // only sent to refresh endpoint
         refreshCookie.setMaxAge(2 * 60); // 2 minutes
-        refreshCookie.setDomain(".george.richmond.gg");
+        refreshCookie.setDomain("george.richmond.gg");
 
         response.addCookie(sessionCookie);
         response.addCookie(refreshCookie);
